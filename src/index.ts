9dc40fec50111caa6c02d35c1ed514cf380a3768
gmelo138 <138888978+gmelo138@users.noreply.github.com>
2024-12-05 23:48:05 -0300

    aaa

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import connectToDatabase from './database'; 
+import workflowRoutes from './routes/workflowRoutes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Workflow Automation!');
-});
+app.use(express.json()); 
+
+connectToDatabase();
+
+app.use('/api', workflowRoutes); 
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
